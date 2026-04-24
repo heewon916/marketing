b92d1b5d2cfc0aea15e602904b13a28e26210139
JuseongYu <juseongyu56@gmail.com>
2026-04-24 16:07:27 +0900

    chore(setting): tainwind 초기 세팅 (S14P31A401-17)

--- fe/src/pages/landing/LandingPage.jsx
+++ fe/src/pages/landing/LandingPage.jsx
@@ -1,5 +1,11 @@
 function LandingPage() {
-  return <div>hisdasfszdfgvdxfcs</div>
+
+
+  return (
+    <main className="min-h-screen bg-surface-base text-primary-500">
+      Landing Page12213123213
+    </main>
+  )
 }
 
 export default LandingPage
