ab8ff7a9549ea2dc3b60f98d297076836b91b2f0
김희원 <kimhiwon@naver.com>
2026-04-29 10:14:03 +0900

    [Back/feat] 파일명 에러 수정

--- be/src/main/java/com/matketing/be/global/common/HealthController.java
+++ be/src/main/java/com/matketing/be/global/common/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HealthCheckController {
+public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
