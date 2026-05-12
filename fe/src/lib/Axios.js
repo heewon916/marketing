dcc1a19ba47add9c9de7b69d02289744bb15d37f
JuseongYu <juseongyu56@gmail.com>
2026-05-12 16:49:39 +0900

    fix(setting): 주석 잘못한것 해제 (S14P31A401-149)

--- fe/src/lib/Axios.js
+++ fe/src/lib/Axios.js
@@ -47,10 +47,10 @@ api.interceptors.response.use(
 
         return api(originalRequest);
       } catch (refreshError) {
-        // localStorage.removeItem('accessToken');
-        // window.location.href = '/';
-//
-        // return Promise.reject(refreshError);
+        localStorage.removeItem('accessToken');
+        window.location.href = '/';
+
+        return Promise.reject(refreshError);
       }
     }
 
